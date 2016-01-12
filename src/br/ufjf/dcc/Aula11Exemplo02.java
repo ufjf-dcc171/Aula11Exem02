27a5232556f2e3720974183b35d9d5f14e05299f
Igor Knop <igorknop@gmail.com>
2016-01-12 21:12:34 -0200

    Commit Inicial

---
+++ src/br/ufjf/dcc/Aula11Exemplo02.java
@@ -0,0 +1,11 @@
+package br.ufjf.dcc;
+public class Aula11Exemplo02 {
+
+    public static void main(String[] args) {
+        ArquivoTexto janela = new ArquivoTexto();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+        
+    }
+    
+}
